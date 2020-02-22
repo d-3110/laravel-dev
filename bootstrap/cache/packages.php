2c318669d0756292ee148626397cd2cd7ac0109a
daisuke saito <higashiishiyama.5983@gmail.com>
2020-02-22 21:00:03 +0900

    composer updategit a .git a .

--- bootstrap/cache/packages.php
+++ bootstrap/cache/packages.php
@@ -28,24 +28,6 @@
       0 => 'Fideloper\\Proxy\\TrustedProxyServiceProvider',
     ),
   ),
-  'intervention/image' => 
-  array (
-    'providers' => 
-    array (
-      0 => 'Intervention\\Image\\ImageServiceProvider',
-    ),
-    'aliases' => 
-    array (
-      'Image' => 'Intervention\\Image\\Facades\\Image',
-    ),
-  ),
-  'kyslik/column-sortable' => 
-  array (
-    'providers' => 
-    array (
-      0 => 'Kyslik\\ColumnSortable\\ColumnSortableServiceProvider',
-    ),
-  ),
   'laravel/tinker' => 
   array (
     'providers' => 
